4c5e503ee16e2098c3a8283db24e86fd9831041e
ByeongdoChoi <cookie023@naver.com>
2021-01-12 13:54:21 +0900

    메인, 결과 페이지 리액트로 결합

--- client/src/resultpage.js
+++ client/src/resultpage.js
@@ -1,17 +1,18 @@
-import './App.css';
+import './RPcss.css';
 import img from './img/empty_image.PNG';
-
+import facebook from './img/facebookicon.PNG';
+import twitter from './img/twittericon.PNG';
+import instagram from './img/instagramicon.PNG';
 function App() {
-  const st_img_style ={
-    width: "220px",
-    height: "267px",
-    marginRight: "40px"
+  const sns_img_style ={
+    marginRight: "50px"
   }
   const ov_img_style ={
     width: "80px",
     heigh: "80px",
     marginRight: "10px"
   }
+  
   return (
     <div>
     <header>
@@ -32,19 +33,22 @@ function App() {
         </li>
       </ul>
     </header>
-    <div className="upload">
-      <h2>Image Upload</h2>
-      <img src={img} alt="empty_image"/> <br/><br/><br/>
-      <button>Upload</button> <br/><br/><br/><br/>
+    <div className="result">
+      <h2>Image Result</h2>
+      <img src={img} alt="empty_image"/> 
+      <img src={img} alt="empty_image"/>
+      <br/><br/>
+      <button style={sns_img_style}>Save</button>
+      <button>Retry</button>
+      <br/><br/><br/>
     </div>
-    <div className="select">
+    <div className="sns">
       <br/><br/>
-      <img src={img} alt="empty_image" style={st_img_style}/>
-      <img src={img} alt="empty_image" style={st_img_style}/>
-      <img src={img} alt="empty_image" style={st_img_style}/>
-      <img src={img} alt="empty_image" style={st_img_style}/>
+      <img src={twitter} alt="empty_image" style={sns_img_style}/>
+      <img src={facebook} alt="empty_image" style={sns_img_style}/>
+      <img src={instagram} alt="empty_image" style={sns_img_style}/>
       <br/><br/>
-      <button>Convert</button>
+      <button>Convert</button>      
     </div>
     <div className="overview">
       <h2>Overveiw</h2><br/>
